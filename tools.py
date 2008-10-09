8c950fd1699e42f6d63958eb60a1d5ce02212ebc
yomguy <>
2008-10-09 13:58:43 +0000

    Add get_ip_address()

--- tools.py
+++ tools.py
@@ -28,6 +28,17 @@ import string
 import time
 from xmltodict import *
 from mutagen.oggvorbis import OggVorbis
+import socket
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 def get_lines(file):
 	"""Get lines from a file"""
