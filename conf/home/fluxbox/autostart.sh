7f37a876c44a8ae1f93d4f85998a5f0bf6af7c43
yomguy <>
2010-07-08 17:42:42 +0000

    fix vncserver for RT priorities, fix deefuzzer PB conf, start edcast with fluxbox in vncserver

--- conf/home/fluxbox/autostart.sh
+++ conf/home/fluxbox/autostart.sh
@@ -14,3 +14,17 @@ fi
 
 qjackctl &
 
+edcast_jack -c /etc/telecaster/edcast_jack_local.cfg -n LIVE -p jack_rack &
+sleep 3
+
+# MONO setup
+jack_disconnect jack_rack:out_2 LIVE:in_2
+jack_connect jack_rack:out_1  LIVE:in_1
+jack_connect jack_rack:out_1  LIVE:in_2
+
+# STEREO setup
+#jack_connect jack_rack:out_1  LIVE:in_1
+#jack_connect jack_rack:out_2  LIVE:in_2
+
+
+
