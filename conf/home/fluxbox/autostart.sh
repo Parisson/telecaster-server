2c582da6c428b172fda6d8748475c58c0cb034b5
yomguy <>
2010-07-08 22:45:09 +0000

    mv conf files

--- conf/home/fluxbox/autostart.sh
+++ conf/home/fluxbox/autostart.sh
@@ -6,7 +6,7 @@ pid=`pgrep jack-rack`
 if [ ! $pid = "" ]; then 
  sleep 1
 else
- jack-rack -n /home/prebarreau/rack/comp_limit_2_02.rack &
+ jack-rack -n /etc/telecaster/eq_comp_limit_02.rack &
  sleep 10
  jack_connect system:capture_1 jack_rack:in_1
  jack_connect system:capture_2 jack_rack:in_2
