07b69d830ef487e586b937b94a05b40006071f18
yomguy <yomguy@parisson.com>
2014-01-13 16:04:32 +0100

    upgrade video size ao 480p, add zoom 140

--- tcserver/conf/usr/local/share/telecaster/scripts/tc_video_simple_webm_stream.sh
+++ tcserver/conf/usr/local/share/telecaster/scripts/tc_video_simple_webm_stream.sh
@@ -2,18 +2,21 @@
 
 # Start TeleCaster video channel
 
-WIDTH=640
-HEIGHT=360
+#WIDTH=640
+#HEIGHT=360
+WIDTH=864
+HEIGHT=480
 #WIDTH=1280
 #HEIGHT=720
 
-v4l2-ctl -d 0 -c power_line_frequency=1
+v4l2-ctl -d 0 -c power_line_frequency=1 -c zoom_absolute=140
 
-gst-launch v4l2src device=/dev/video0 ! video/x-raw-yuv, width=$WIDTH, height=$HEIGHT, framerate={30/1}  \
+gst-launch v4l2src device=/dev/video0 ! video/x-raw-rgb, width=$WIDTH, height=$HEIGHT, framerate={30/1}  \
+        ! queue ! videoflip method=rotate-180 \
 	! queue ! ffmpegcolorspace \
-	! queue ! vp8enc speed=2 threads=4 quality=10.0 max-latency=25 max-keyframe-distance=96 auto-alt-ref-frames=true  ! queue ! muxout. \
+	! queue ! vp8enc speed=2 threads=4 quality=10.0 max-latency=25 max-keyframe-distance=30 auto-alt-ref-frames=true  ! queue ! muxout. \
 	jackaudiosrc connect=2 ! audio/x-raw-float, channels=2 \
-	! queue ! audioconvert ! queue ! vorbisenc quality=0.3 ! queue ! muxout.  \
+	! queue ! audioconvert ! queue ! vorbisenc quality=0.4 ! queue ! muxout.  \
 	webmmux streamable=true name=muxout \
-	! queue ! tcpserversink host=127.0.0.1 port=9000 protocol=none blocksize=65536 sync-method=1 \
+	! tcpserversink host=127.0.0.1 port=9000 protocol=none blocksize=65536 sync-method=1 \
 	> /dev/null
