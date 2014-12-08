1a7a1f445c997924555fe0abd2832d90730acfd8
Guillaume Pellerin <yomguy@parisson.com>
2014-12-08 14:39:34 +0100

    again

--- conf/home/telecaster/.fluxbox/autostart.sh
+++ conf/home/telecaster/.fluxbox/autostart.sh
@@ -24,30 +24,28 @@ scripts/tc_audio_mp3_icecast.sh &
 
 sleep 2
 
-# MONO
-#jack_connect    jack_rack:out_1 lamemp3enc:in_jackaudiosrc0_2
+jack_disconnect system:capture_1 gst-launch-0.10:in_jackaudiosrc0_1
+jack_disconnect system:capture_2 gst-launch-0.10:in_jackaudiosrc0_2
+
+jack_connect jack_rack:out_1 gst-launch-0.10:in_jackaudiosrc0_1
+jack_connect jack_rack:out_2 gst-launch-0.10:in_jackaudiosrc0_2
+
 #jack_connect jack_rack:out_1  system:playback_1
 #jack_connect jack_rack:out_1  system:playback_2
 
 # STEREO setup
-jack_disconnect system:capture_1 gst-launch-0.10:in_jackaudiosrc0_1
-jack_disconnect system:capture_2 gst-launch-0.10:in_jackaudiosrc0_2
-
-jack_connect    jack_rack:out_1 gst-launch-0.10:in_jackaudiosrc0_1
-jack_connect    jack_rack:out_2 gst-launch-0.10:in_jackaudiosrc0_2
-
 #jack_connect jack_rack:out_1  lamemp3enc:in_1
 #jack_connect jack_rack:out_2  lamemp3enc:in_2
 #jack_connect jack_rack:out_1  system:playback_1
 #jack_connect jack_rack:out_2  system:playback_2
 
-# 4 channels setup
-#jack_connect system:capture_1 jack_rack:in_1
-#jack_connect system:capture_1 jack_rack:in_2
-#jack_connect system:capture_2 jack_rack:in_1
-#jack_connect system:capture_2 jack_rack:in_2
-#jack_connect system:capture_3 jack_rack:in_1
-#jack_connect system:capture_4 jack_rack:in_2
+ # 4 channels setup
+ #jack_connect system:capture_1 jack_rack:in_1
+ #jack_connect system:capture_1 jack_rack:in_2
+ #jack_connect system:capture_2 jack_rack:in_1
+ #jack_connect system:capture_2 jack_rack:in_2
+ #jack_connect system:capture_3 jack_rack:in_1
+ #jack_connect system:capture_4 jack_rack:in_2
 
 # ---------------------
 # Video channel
