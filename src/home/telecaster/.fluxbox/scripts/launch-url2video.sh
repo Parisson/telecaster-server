3fe53859cf26a92a3d6c77edcb6292c17845818f
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2021-11-25 03:11:08 +0100

    add audio queue, max-buffer=2, fix pa name

--- src/home/telecaster/.fluxbox/scripts/launch-url2video.sh
+++ src/home/telecaster/.fluxbox/scripts/launch-url2video.sh
@@ -3,11 +3,12 @@
 URL=https://e-learning.crfpa.pre-barreau.com/media/Pre-Barreau/CRFPA/2021/Libertes_-_Cours/3cf774cb990d9987/crfpa-libertes-cours-09_16_21-09:45:47.mp4
 
 VIDEO_SINK_NAME="/dev/video12"
-AUDIO_SINK_NAME="Mix-for-Virtual-Microphone"
+AUDIO_SINK_NAME="mix-for-virtual-mic"
 
 gst-launch-1.0 uridecodebin uri="$URL" name=uridec do-timestamp=true live=true \
       ! videoconvert \
       ! v4l2sink device=$VIDEO_SINK_NAME sync=true \
       uridec. \
+      ! queue \
       ! audioconvert \
       ! pulsesink device=$AUDIO_SINK_NAME sync=true
--- src/home/telecaster/.fluxbox/scripts/launch-v4l2loopback.sh
+++ src/home/telecaster/.fluxbox/scripts/launch-v4l2loopback.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo modprobe v4l2loopback devices=1 video_nr=12 card_label="virtual webcam" exclusive_caps=1
+sudo modprobe v4l2loopback devices=1 video_nr=12 card_label="virtual webcam" exclusive_caps=1 max_buffers=2
